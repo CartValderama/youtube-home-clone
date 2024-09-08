734885ef67920b6974b2b2e23f6c48d1723cdd3d
Cart Valderama <cartantonio2000@gmail.com>
2024-09-08 20:26:14 +0200

    add the category pills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import CategoryPills from "./component/CategoryPills";
 import PageHeader from "./layouts/PageHeader";
+import { categories } from "./data/home";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader></PageHeader>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            ></CategoryPills>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
